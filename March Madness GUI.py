88339931f17b14e4e2752f8bd47855c675d65c97
Antonio Thomas Atkinson <atatkins@umich.edu>
2018-03-03 00:50:01 -0500

    Added bpi file and created textbox in html form

--- March Madness GUI.py
+++ March Madness GUI.py
@@ -1,7 +1,9 @@
 from flask import Flask
 import flask
+from flask import request
 from kenpom import kenpom_rankings
 from sagarin import sagarin_rankings
+from bpi import bpi_rankings
 import psycopg2 as p
 import urllib
 from bs4 import BeautifulSoup
@@ -15,76 +17,36 @@ from secrets import port
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def hello_world():
-    context = {}
-    context["data"] = []
-
-    # con = p.connect("dbname=" + dbname +" user=" + username + " password=" + password + " host=" + host + " port=" + port)
     con = p.connect("dbname=demo user='postgres' password='' host='localhost' port=5433")
     cur = con.cursor()
-    cur.execute("select * from test_table")
-    rows = cur.fetchall()
+    context = {}
 
-    # Alias of certain team names
-    team_alias = {}
-    team_alias["Saint Mary's"] = "Saint Mary's-Cal."
-    team_alias["Xavier"] = "Xavier-Ohio"
-    team_alias["Miami FL"] = "Miami-Florida"
-    team_alias["VCU"] = "VCU(Va. Commonwealth)"
-    team_alias["USC"] = "Southern California"
-    team_alias["UNC Wilmington"] = "NC Wilmington"
-    team_alias["North Carolina State"] = "NC State"
-    team_alias["UNC Asheville"] = "NC Asheville"
-    team_alias["Loyola MD"] = "Loyola-Maryland"
-    team_alias["LIU Brooklyn"] = "Long Island U."
-    team_alias["UConn"] = "Connecticut"
+    if request.method == 'POST':
+        if 'search' in request.form:
+            text = request.form["search"]
+            text2 = request.form["search2"]
+            cur.execute("select * from input_data WHERE name=%s", [text])
+            data = cur.fetchone()
+            cur.execute("select * from input_data WHERE name=%s", [text2])
+            print(cur.fetchone())
 
-    i = 1
-    N = 15
-    # BPI rankings
-    while i <= N:
-        # url = "http://www.espn.com/mens-college-basketball/bpi/_/season/2012/view/overview"
-        url = "http://www.espn.com/mens-college-basketball/bpi/_/view/bpi/season/2012/page/"+str(i)
-        i+=1
-        page = urllib.request.urlopen(url).read()
+            context["team"] = data[0]
+            context["seed"] = data[1]
+            context["bpi"] = data[5]
+            context["bpirank"] = data[6]
+            context["kenpom"] = data[7]
+            context["kenpomrank"] = data[8]
+            context["sagarin"] = data[9]
+            context["sagarinrank"] = data[10]
+            print("Searching...", text)
+        if 'update-data' in request.form:
+            kenpom_rankings()
+            sagarin_rankings()
+            bpi_rankings()
+            print("Updating...")
 
-        soup = BeautifulSoup(page, 'html.parser')
-
-        # # Grab all the table rows and put it into a list
-        for row in soup.find_all("tr")[2:]:
-            names = row.find_all("span")
-            team_data = row.find_all("td")
-            del team_data[1]
-
-            bpi_rank = team_data[0].get_text()
-            bpi_rating = team_data[-2].get_text()
-            team_name = names[1].get_text()
-
-            # Check to see if there are any trailing white spaces
-            if team_name[-1] == " ":
-                team_name = team_name[0:-1]
-
-            # Check to see if team name ends in "St." and replace it with "State"
-            if team_name[-5:] == "State":
-                temp = team_name.split()
-                temp[-1] = "St."
-                print("temp", temp)
-                team_name = " ".join(temp)
-            # Check for an alias
-            if team_name in team_alias:
-                team_name = team_alias[team_name]
-
-            print("name", names[1].get_text())
-            print("bpi ranking", bpi_rank)
-            print("bpi rating", bpi_rating)
-            print("")
-
-            cur.execute("UPDATE input_data SET bpi=%s, bpirank=%s WHERE name=%s",
-                    [float(bpi_rating), int(bpi_rank), team_name])
-
-
-        con.commit()
 
     return flask.render_template("hello.html", **context)
 
---
+++ bpi.py
@@ -0,0 +1,80 @@
+from flask import Flask
+import flask
+from kenpom import kenpom_rankings
+from sagarin import sagarin_rankings
+import psycopg2 as p
+import urllib
+from bs4 import BeautifulSoup
+from secrets import dbname
+from secrets import username
+from secrets import password
+from secrets import host
+from secrets import port
+def bpi_rankings():
+    context = {}
+    context["data"] = []
+
+    # con = p.connect("dbname=" + dbname +" user=" + username + " password=" + password + " host=" + host + " port=" + port)
+    con = p.connect("dbname=demo user='postgres' password='' host='localhost' port=5433")
+    cur = con.cursor()
+    cur.execute("select * from test_table")
+    rows = cur.fetchall()
+
+    # Alias of certain team names
+    team_alias = {}
+    team_alias["Saint Mary's"] = "Saint Mary's-Cal."
+    team_alias["Xavier"] = "Xavier-Ohio"
+    team_alias["Miami FL"] = "Miami-Florida"
+    team_alias["VCU"] = "VCU(Va. Commonwealth)"
+    team_alias["USC"] = "Southern California"
+    team_alias["UNC Wilmington"] = "NC Wilmington"
+    team_alias["North Carolina State"] = "NC State"
+    team_alias["UNC Asheville"] = "NC Asheville"
+    team_alias["Loyola MD"] = "Loyola-Maryland"
+    team_alias["LIU Brooklyn"] = "Long Island U."
+    team_alias["UConn"] = "Connecticut"
+
+    i = 1
+    N = 15
+    # BPI rankings
+    while i <= N:
+        # url = "http://www.espn.com/mens-college-basketball/bpi/_/season/2012/view/overview"
+        url = "http://www.espn.com/mens-college-basketball/bpi/_/view/bpi/season/2012/page/"+ str(i)
+        i += 1
+        page = urllib.request.urlopen(url).read()
+
+        soup = BeautifulSoup(page, 'html.parser')
+
+        # # Grab all the table rows and put it into a list
+        for row in soup.find_all("tr")[2:]:
+            names = row.find_all("span")
+            team_data = row.find_all("td")
+            del team_data[1]
+
+            bpi_rank = team_data[0].get_text()
+            bpi_rating = team_data[-2].get_text()
+            team_name = names[1].get_text()
+
+            # Check to see if there are any trailing white spaces
+            if team_name[-1] == " ":
+                team_name = team_name[0:-1]
+
+            # Check to see if team name ends in "St." and replace it with "State"
+            if team_name[-5:] == "State":
+                temp = team_name.split()
+                temp[-1] = "St."
+                print("temp", temp)
+                team_name = " ".join(temp)
+            # Check for an alias
+            if team_name in team_alias:
+                team_name = team_alias[team_name]
+
+            print("name", names[1].get_text())
+            print("bpi ranking", bpi_rank)
+            print("bpi rating", bpi_rating)
+            print("")
+
+            cur.execute("UPDATE input_data SET bpi=%s, bpirank=%s WHERE name=%s",
+                        [float(bpi_rating), int(bpi_rank), team_name])
+
+        con.commit()
